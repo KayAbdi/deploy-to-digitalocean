19704086bf81eacd439eb37d556ff13860ae239e
Kienaan Abdi <abdifiiq@google.com>
2022-08-11 23:01:00 -0500

    changes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,13 +4,13 @@
         connection: {
           client: 'postgres',
           connection: {
-            host: env('DATABASE_HOST'),
-            port: env.int('DATABASE_PORT'),
-            database: env('DATABASE_NAME'),
-            user: env('DATABASE_USERNAME'),
-            password: env('DATABASE_PASSWORD'),
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD'), 'public',
             ssl: {
-              rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
             },
           },
           debug: false,
